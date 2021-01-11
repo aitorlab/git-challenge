493474bff6d325ba53ba3f97ff0259f99ffd69d5
Aitor <aitor.moreno@hotmail.com>
2021-01-11 19:19:17 +0100

    pasapalabra

---
+++ pasapalabra/pasapalabra.js
@@ -0,0 +1,290 @@
+let questions = [
+
+    [{ letter: "A", answer: "ABDUCIR", status: 0, question: "CON LA A. Dicho de una supuesta criatura extraterrestre: Apoderarse de alguien"},
+
+     { letter: "A", answer: "AMENAZA", status: 0, question: "CON LA A. Cosa o persona que constituye una posible causa de riesgo o perjuicio para alguien o algo"},
+
+     { letter: "A", answer: "ASTRO", status: 0, question: "CON LA A. Cuerpo celeste"}],
+
+    [{ letter: "B", answer: "BINGO", status: 0, question: "CON LA B. Juego que ha sacado de quicio a todos los 'Skylabers' en las sesiones de precurso"},
+
+     { letter: "B", answer: "BALON", status: 0, question: "CON LA B. Pelota grande llena de aire que se utiliza para jugar o para practicar determinados deportes"},
+
+     { letter: "B", answer: "BOOTSTRAP", status: 0, question: "CON LA B. Conocido framework CSS de código abierto"}],
+
+    [{ letter: "C", answer: "CHURUMBEL", status: 0, question: "CON LA C. Niño, crío, bebé"},
+
+     { letter: "C", answer: "CASA", status: 0, question: "CON LA C. Construcción cubierta destinada a ser habitada"},
+
+     { letter: "C", answer: "COCTEL", status: 0, question: "CON LA C. Bebida alcohólica que se prepara mezclando licores con jugos u otros ingredientes"}],
+
+    [{ letter: "D", answer: "DIARREA", status: 0, question: "CON LA D. Anormalidad en la función del aparato digestivo caracterizada por frecuentes evacuaciones y su consistencia líquida"},
+
+     { letter: "D", answer: "DEDAL", status: 0, question: "CON LA D. Utensilio de costura que se ajusta al extremo del dedo"},
+
+     { letter: "D", answer: "DONUT", status: 0, question: "CON LA D. Bollo, generalmente en forma de rosquilla"}],
+
+    [{ letter: "E", answer: "ECTOPLASMA", status: 0, question: "CON LA E. Gelatinoso y se encuentra debajo de la membrana plasmática. Los cazafantasmas medían su radiación"},
+
+     { letter: "E", answer: "EMPRESA", status: 0, question: "CON LA E. Entidad en la que se realizan actividades de producción industrial o de prestación de servicios"},
+
+     { letter: "E", answer: "ERMITAÑO", status: 0, question: "CON LA E. Persona que vive sola en un lugar deshabitado, especialmente para dedicar su vida a la oración y al sacrificio"}],
+
+    [{ letter: "F", answer: "FACIL", status: 0, question: "CON LA F. Que no requiere gran esfuerzo, capacidad o dificultad"},
+
+     { letter: "F", answer: "FRUSTRACION", status: 0, question: "CON LA F. Imposibilidad de satisfacer una necesidad o un deseo"},
+
+     { letter: "F", answer: "FILIA", status: 0, question: "CON LA F. Simpatía o afición por una determinada persona o cosa"}],
+     
+    [{ letter: "G", answer: "GATO", status: 0, question: "CON LA G. Instrumento compuesto por un engranaje de piñón y cremallera que sirve para levantar a poca altura grandes pesos"},
+
+     { letter: "G", answer: "GALAXIA", status: 0, question: "CON LA G. Conjunto enorme de estrellas, polvo interestelar, gases y partículas"},
+
+     { letter: "G", answer: "GOTA", status: 0, question: "CON LA G. Porción pequeña y con forma más o menos esférica de una sustancia líquida"}],
+
+    [{ letter: "H", answer: "HEROKU", status: 0, question: "CON LA H. Conocida plataforma de hosting que soporta distintos lenguajes de programación"},
+
+     { letter: "H", answer: "HARAKIRI", status: 0, question: "CON LA H. Suicidio ritual japonés por desentrañamiento"},
+
+     { letter: "H", answer: "HEXADECIMAL", status: 0, question: "CON LA H.  Que tiene como base el número 16; se utiliza a menudo para una representación condensada del número binario mediante cadenas de 4 bits"}],
+
+    [{ letter: "I", answer: "INFORMATICA", status: 0, question: "CON LA I. Conjunto de conocimientos técnicos que se ocupan del tratamiento automático de la información por medio de computadoras"},
+
+     { letter: "I", answer: "INDUSTRIAL", status: 0, question: "CON LA I. De la industria o relacionado con esta actividad económica"},
+
+     { letter: "I", answer: "INTERNET", status: 0, question: "CON LA I. Red informática de nivel mundial que utiliza la línea telefónica para transmitir la información"}],
+
+    [{ letter: "J", answer: "JABALI", status: 0, question: "CON LA J. Variedad salvaje del cerdo que sale en la película 'El Rey León', de nombre Pumba"},
+
+     { letter: "J", answer: "JIRON", status: 0, question: "CON LA J. Trozo desgarrado o arrancado de una tela o de una prenda de vestir"},
+
+     { letter: "J", answer: "JOROBA", status: 0, question: "CON LA J. Abultamiento o protuberancia que tienen en el lomo ciertos animales formado por acumulación de grasas"}],
+
+    [{ letter: "K", answer: "KAMIKAZE", status: 0, question: "CON LA K. Persona que se juega la vida realizando una acción temeraria"},
+
+     { letter: "K", answer: "KOALA", status: 0, question: "CON LA K. Mamífero conocido por sus movimientos lentos, es arborícola y vive en Australia."},
+
+     { letter: "K", answer: "KARATE", status: 0, question: "CON LA K. Sistema de combate sin armas de origen japonés, hoy practicado principalmente como deporte"}],
+
+    [{ letter: "L", answer: "LICANTROPO", status: 0, question: "CON LA L. Hombre lobo"},
+
+     { letter: "L", answer: "LABRADOR", status: 0, question: "CON LA L. Persona que tiene por oficio trabajar y cultivar la tierra, en especial si es el propietario de esta"},
+
+     { letter: "L", answer: "LINUX", status: 0, question: "CON LA L. Sistema operativo de código abierto creado por Linus Torvalds"}],
+
+    [{ letter: "M", answer: "MISANTROPO", status: 0, question: "CON LA M. Persona que huye del trato con otras personas o siente gran aversión hacia ellas"},
+
+     { letter: "M", answer: "MOTEL", status: 0, question: "CON LA M. Establecimiento de hostelería situado cerca de una carretera que dispone de apartamentos con garaje y entrada independiente para viajeros de paso o estancias de corta duración"},
+
+     { letter: "M", answer: "MALEANTE", status: 0, question: "CON LA M. Que vive de forma marginal cometiendo acciones delictivas de manera habitual"}],
+
+    [{ letter: "N", answer: "NECEDAD", status: 0, question: "CON LA N. Demostración de poca inteligencia"},
+
+     { letter: "N", answer: "NUTRIENTE", status: 0, question: "CON LA N. Que nutre o alimenta"},
+
+     { letter: "N", answer: "NOTARIO", status: 0, question: "CON LA N. Funcionario público que tiene autoridad para dar fe de los actos públicos realizados ante él"}],
+
+    [{ letter: "Ñ", answer: "SEÑAL", status: 0, question: "CONTIENE LA Ñ. Indicio que permite deducir algo de lo que no se tiene un conocimiento directo."},
+
+     { letter: "Ñ", answer: "PATRAÑA", status: 0, question: "CONTIENE LA Ñ. Mentira o falsedad grande y complicada que se cuenta o se dice a alguien"},
+
+     { letter: "Ñ", answer: "AÑO", status: 0, question: "CONTIENE LA Ñ. Tiempo que emplea la Tierra en dar una vuelta alrededor del Sol"}],
+
+    [{ letter: "O", answer: "ORCO", status: 0, question: "CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien"},
+
+     { letter: "O", answer: "OMOPLATO", status: 0, question: "CON LA O. Hueso ancho, aplanado y de forma aproximadamente triangular que está situado en la parte superior de la espalda"},
+
+     { letter: "O", answer: "OTOÑO", status: 0, question: "CON LA O. Estación del año comprendida entre el verano y el invierno"}],
+
+    [{ letter: "P", answer: "PROTOSS", status: 0, question: "CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft"},
+
+     { letter: "P", answer: "PANAL", status: 0, question: "CON LA P. Estructura de cera formada por multitud de pequeñas celdas o casillas de forma hexagonal construida por abejas"},
+
+     { letter: "P", answer: "PARRA", status: 0, question: "CON LA P. Planta de vid que se deja crecer y se sostiene sobre un soporte pegado a la pared o sobre un armazón"}],
+
+    [{ letter: "Q", answer: "QUIOSCO", status: 0, question: "CON LA Q. Local comercial de pequeño tamaño destinado a la venta de periódicos, revistas, golosinas y otros artículos"},
+
+     { letter: "Q", answer: "QUESO", status: 0, question: "CON LA Q. Producto obtenido por la maduración de la cuajada de la leche"},
+
+     { letter: "Q", answer: "PARQUE", status: 0, question: "CONTIENE LA Q. Terreno acotado en núcleos rurales o urbanos, generalmente con plantas y árboles, destinado a usos diversos, especialmente al recreo público"}],
+
+    [{ letter: "R", answer: "RATON", status: 0, question: "CON LA R. Roedor"},
+
+     { letter: "R", answer: "ROMA", status: 0, question: "CON LA R. Capital de Italia"},
+
+     { letter: "R", answer: "REMO", status: 0, question: "CON LA R. pala larga y estrecha de madera que permite mover o hacer avanzar una embarcación haciendo con ella fuerza en el agua"}],
+
+    [{ letter: "S", answer: "STACKOVERFLOW", status: 0, question: "CON LA S. Comunidad salvadora de todo desarrollador informático"},
+
+     { letter: "S", answer: "SUDOKU", status: 0, question: "CON LA S. Pasatiempo que consiste en rellenar con números del 1 al 9 las casillas en blanco de una cuadrícula"},
+
+     { letter: "S", answer: "SAPO", status: 0, question: "CON LA S. Anfibio de cuerpo más grueso que la rana y con la piel sembrada de verrugas"}],
+
+    [{ letter: "T", answer: "TERMINATOR", status: 0, question: "CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984"},
+
+     { letter: "T", answer: "TORNADO", status: 0, question: "CON LA T. Gigantesco embudo de aire, que sopla en espiral ciclónica hacia arriba, girando a modo de torbellino"},
+
+     { letter: "T", answer: "TETERA", status: 0, question: "CON LA T. Recipiente para preparar y servir el té, con una tapadera y un pitorro para verterlo"}],
+
+    [{ letter: "U", answer: "UNAMUNO", status: 0, question: "CON LA U. Escritor y filósofo español de la generación del 98 autor del libro 'Niebla' en 1914"},
+
+     { letter: "U", answer: "UTENSILIO", status: 0, question: "CON LA U. Objeto fabricado que es a propósito para un determinado uso, en especial si es un uso frecuente como el doméstico o artesanal"},
+
+     { letter: "U", answer: "URBE", status: 0, question: "CON LA U. Ciudad, especialmente la que tiene un gran número de habitantes"}],
+
+    [{ letter: "V", answer: "VIKINGOS", status: 0, question: "CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa"},
+
+     { letter: "V", answer: "VOLTAJE", status: 0, question: "CON LA V. Potencial eléctrico, expresado en voltios"},
+
+     { letter: "V", answer: "VERSO", status: 0, question: "CON LA V. Enunciado o conjunto de palabras que forma una unidad en un poema, sujeto a ritmo y a medida determinados"}],
+
+    [{ letter: "W", answer: "SANDWICH", status: 0, question: "CONTIENE LA W. Emparedado hecho con dos rebanadas de pan entre las cuales se coloca jamón y queso"},
+
+     { letter: "W", answer: "WEB", status: 0, question: "CON LA W. Conjunto de información que se encuentra en una dirección determinada de internet"},
+
+     { letter: "W", answer: "HARDWARE", status: 0, question: "CONTIENE LA W. Conjunto de elementos físicos o materiales que constituyen una computadora o un sistema informático"}],
+
+    [{ letter: "X", answer: "BOTOX", status: 0, question: "CONTIENE LA X. Toxina bacteriana utilizada en cirujía estética"},
+
+     { letter: "X", answer: "ANEXO", status: 0, question: "CONTIENE LA X. Que va unido a otra cosa de la cual depende o con la que está muy relacionado"},
+
+     { letter: "X", answer: "BUROFAX", status: 0, question: "CONTIENE LA X. Documento legal que solo se puede gestionar a través de este servicio"}],
+
+    [{ letter: "Y", answer: "PEYOTE", status: 0, question: "CONTIENE LA Y. Pequeño cáctus conocido por sus alcaloides psicoactivos utilizado de forma ritual y medicinal  por indígenas americanos"},
+
+     { letter: "Y", answer: "BAYA", status: 0, question: "CONTIENE LA Y. Fruto carnoso o pulposo con varias semillas en su interior que están envueltas directamente por la pulpa"},
+
+     { letter: "Y", answer: "BYTE", status: 0, question: "CONTIENE LA Y. Conjunto de 8 bits que recibe el tratamiento de una unidad y que constituye el mínimo elemento de memoria direccionable de una computadora"}],
+
+    [{ letter: "Z", answer: "ZEN", status: 0, question: "CON LA Z. Escuela de budismo que busca la experiencia de la sabiduría más allá del discurso racional"},
+
+     { letter: "Z", answer: "ACIDEZ", status: 0, question: "CONTIENE LA Z. Término que indica la cantidad de ácido en una sustancia"},
+
+     { letter: "Z", answer: "BUZON", status: 0, question: "CONTIENE LA Z. Recipiente con una abertura longitudinal donde se depositan determinados papeles, en especial la correspondencia"}]
+];
+
+let rankingList = [
+    {name: 'Jhon', puntuaction: 800},
+    {name: 'Alice', puntuaction: 5000}, 
+    {name: 'Jack', puntuaction: 600},
+    {name: 'Peter', puntuaction: 200}, 
+    {name: 'Bob', puntuaction: 50}    
+];
+
+function pasapalabra() {
+    let name;
+    let correctAnswers = 0;
+    let incorrectAnswers = 0;  
+    let statusArray = [];
+    let questionsPanel = [];
+    let userAnswer;
+
+    function getName() {
+        name = prompt('Bienvenido a Pasapalabra! Por favor, introduce tu nombre');
+
+        if(typeof(name) === 'string' && name !== "") {
+            alert(`${name}, debido a que el objetivo del juego es adivinar las palabras y para evitar confusiones, ` +
+            `las respuestas las deberás informar sin acentos. Para pasar una palabra, escribe "pasapalabra" o pulsa el botón "OK" al ver la pregunta. ` +
+            `Si en cualquier momento deseas finalizar la partida antes de tiempo, escribe "end" o pulsa el botón "Cancel" al ver la pregunta. Empecemos!`);
+            return;
+        } else {
+            alert("Por favor, intrduce un nombre válido");
+            getName();
+        }
+    }
+    
+    function createPanel() {
+        let randomIndex = 0;
+
+        for(let i = 0; i < questions.length; i++) {
+            randomIndex = Math.floor(Math.random() * questions[i].length);
+            questionsPanel.push(questions[i][randomIndex]);
+        }
+    }
+
+    function askQuestions(arrayQuestions) {
+
+        for(let i = 0; i < arrayQuestions.length; i++) {
+            userAnswer = prompt(arrayQuestions[i].question);
+            if(typeof(userAnswer) === 'string') {
+                userAnswer = userAnswer.toUpperCase();
+
+                if(userAnswer === arrayQuestions[i].answer) {       
+                    arrayQuestions[i].status = 'correct';
+                    correctAnswers++;
+                } else if(userAnswer === 'PASAPALABRA' || userAnswer === "") {
+                    arrayQuestions[i].status = 'postponed';       
+                } else if(userAnswer === 'end') {
+                    return;
+                } else if(userAnswer !== arrayQuestions[i].answer && userAnswer !== "") {            
+                    arrayQuestions[i].status = 'incorrect';
+                    incorrectAnswers++;
+                } else {
+                    return;
+                }
+            } else {
+                return;
+            }
+        }
+    }
+
+    function nextRound() {
+        let newQuestions = questionsPanel.filter(question => question.status === 'postponed');
+        alert(`${name}, has finalizado la ronda con ${correctAnswers} aciertos y ${incorrectAnswers} fallos. Continuamos con la siguiente ronda desde la letra ${newQuestions[0].letter}`);
+        askQuestions(newQuestions);
+        checkCompleted(newQuestions);          
+    }
+
+    function checkCompleted(arrayToCheck) {    
+        const statusIsCompleted = (status) => status !== 'postponed' && userAnswer !== 'end' && userAnswer !== null;
+
+        for(let i = 0; i < arrayToCheck.length; i++) {
+            statusArray.push(arrayToCheck[i].status);
+        }
+
+        if(statusArray.every(statusIsCompleted)) {
+            alert(`${name}, has finalizado la partida con ${correctAnswers} aciertos y ${incorrectAnswers} fallos. ` + 
+                  `A contnuación se mostrará el ranking de puntos en la consola. Gracias por jugar a Paspalabra!`);
+            calculateUserPuntuaction();
+            showRanking();
+            return;
+        } else if(userAnswer === 'end' || userAnswer === null) {
+            alert(`${name}, has finalizado la partida con ${correctAnswers} aciertos y ${incorrectAnswers} fallos. Gracias por jugar a Pasapalabra!`);
+        } else {
+            statusArray = [];
+            nextRound();
+        }
+    }
+
+    function calculateUserPuntuaction() {
+        if(correctAnswers === 27) {
+            userPuntuaction = 10000;
+        } else if(correctAnswers >= 20 && correctAnswers < 27) {
+            userPuntuaction = 1000;
+        } else if(correctAnswers >= 10 && correctAnswers < 20) {
+            userPuntuaction = 500;
+        } else if(correctAnswers > 0 && correctAnswers < 10) {
+            userPuntuaction = 100;
+        } else {
+            userPuntuaction = 0;
+        }
+    }
+
+    function showRanking() {
+        rankingList.push({name: name, puntuaction: userPuntuaction});
+
+        rankingList.sort(function(a, b) {
+            return -(a.puntuaction - b.puntuaction);
+        });
+
+        console.table(rankingList);
+    }
+
+    getName();
+    createPanel();
+    askQuestions(questionsPanel);
+    checkCompleted(questionsPanel);
+
+}
+
+pasapalabra();
